a4a2dce8e9a8708c335f03c9ca809287c7d4b454
luohongyu <luohongyu907@qq.com>
2020-09-14 09:26:32 +0800

    修改测试类

--- data-source-gen/src/test/java/org/puddingjk/DataSourceGenApplicationTests.java
+++ data-source-gen/src/test/java/org/puddingjk/DataSourceGenApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OneviewPowerApplicationTests {
+class DataSourceGenApplicationTests {
 
     @Test
     void contextLoads() {
